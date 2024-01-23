65803e35cfd4d90f01411c109a3b836d7081c6a4
nim-m <nimishamajumder@gmail.com>
2024-01-23 16:12:23 +0530

    MinMaxScaler (Accuracy: 0.965)

--- minmax_scaler.py
+++ minmax_scaler.py
@@ -3,6 +3,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
 from sklearn.preprocessing import MinMaxScaler
+import matplotlib.pyplot as plt
 
 
 def load():
