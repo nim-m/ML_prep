ea283afd65e53ba50d880ec32be7e83e71ecdd97
Nimisha <107911764+nim-m@users.noreply.github.com>
2024-02-09 12:48:57 +0530

    XGBRegressor on CBCL with CV - predict ASD t-score
    [0]	validation_0-rmse:8.97564	validation_0-mae:7.15940	validation_1-rmse:8.87573	validation_1-mae:7.18793
    [83]	validation_0-rmse:0.31046	validation_0-mae:0.22558	validation_1-rmse:1.33056	validation_1-mae:0.98172
    
    Cross Validation:
    [0.98152962 0.98066013 0.98640287 0.98104991 0.98670211 0.98104456 0.97906875 0.97828493 0.98098945 0.97539661]

---
+++ xgboost_cbcl.py
@@ -0,0 +1,58 @@
+from xgboost import XGBRegressor
+import pandas as pd
+from sklearn.model_selection import train_test_split, cross_val_score
+
+
+def load_data():
+    df = pd.read_csv('cbcl_1_5-2023-07-21.csv')
+
+    # data cleaning -------------
+
+    df['cbcl_validity_flag'] = df['cbcl_validity_flag'].fillna(0)
+    df = df[(df['cbcl_validity_flag'] != 1)]
+
+    # drop rows where dsm5_autism_spectrum_problems_t_score = NaN
+    df = df.dropna(subset=['dsm5_autism_spectrum_problems_t_score'])
+
+    # Drop rows where any cell in the subset contains NaN
+    subset_columns = df.iloc[:, 11:110]
+    df = df.dropna(subset=subset_columns.columns)
+
+    # define X and y -------------
+    X = df.iloc[:, 11:110].copy()
+    y = df['dsm5_autism_spectrum_problems_t_score']
+
+    return X, y
+
+
+def create_model(eval_metrics):
+    return XGBRegressor(n_estimators=100, learning_rate=0.1, eval_metric=eval_metrics)
+
+
+def cross_validation(model, X, y):
+    # perform cross validation
+    print(results)
+
+
+def main():
+    X, y = load_data()
+
+    # Split the data into training and test sets. Further split the training set into training and validation sets
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.2, random_state=42)
+
+    eval_set = [(X_train, y_train), (X_val, y_val)]
+    eval_metrics = ["rmse", "mae"]
+
+    model = create_model(eval_metrics)
+    model.fit(X_train, y_train, eval_set=eval_set, verbose=True, early_stopping_rounds=2)
+
+    # Perform cross validation
+    cv_results = cross_val_score(model, X_train, y_train, cv=10, scoring="r2")
+
+    print("\nCross Validation:")
+    print(cv_results)
+
+
+if __name__ == "__main__":
+    main()
